d14a128314de4eb4af5e7bf39a72d6f3535a8ceb
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2025-05-13 15:23:57 -0300

    Removido tratamento de erro na main

--- Trabalho 1/main.cpp
+++ Trabalho 1/main.cpp
@@ -8,45 +8,28 @@
  */
 #include "Aresta.h"
 #include "Grafo.h"
-#include <exception>
-#include <string>
 #include <iostream>
 
 using namespace std;
 
-void print_exception(const exception &e, int level = 0) {
-    cerr << "exception: " << string(level, ' ') << e.what() << "\n";
-    try {
-        rethrow_if_nested(e);
-    } catch(const std::exception& nested_exception) {
-        print_exception(nested_exception, (level + 2));
-    }
-}
-
 int main() {
-    try {
-        int a, b = 0;
-        cin >> a >> b;
-        Grafo grafo(a);
+    int a, b = 0;
+    cin >> a >> b;
+    Grafo grafo(a);
 
-        for (int i = 0; i < b; i++) {
-            int v1, v2;
-            char t;
-            cin >> v1 >> v2 >> t;
+    for (int i = 0; i < b; i++) {
+        int v1, v2;
+        char t;
+        cin >> v1 >> v2 >> t;
 
-            if (t == 'A')
-            {
-                grafo.addAresta(Aresta(v1, v2));
-            }
-
+        if (t == 'A')
+        {
+            grafo.addAresta(Aresta(v1, v2));
         }
+    }
 
-        cout << (grafo.eh_bipartido_1() ? "SIM" : "NAO") << endl;
-        cout << (grafo.eh_bipartido_2() ? "SIM" : "NAO") << endl;
-    }
-    catch (const exception &e) {
-        print_exception(e);
-    }
+    cout << (grafo.eh_bipartido_1() ? "SIM" : "NAO") << endl;
+    cout << (grafo.eh_bipartido_2() ? "SIM" : "NAO") << endl;
 
     return 0;
 }
